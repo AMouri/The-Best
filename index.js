40b8de40e4bb19f7cd9aa56204e8a919d23e3973
Alec Mouri <alec.mouri@gmail.com>
2012-10-02 20:48:55 -0700

    adding index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log($);
+
+$(function(){
+	$(".stuffs").css("color", "red");
+	$("h1").css("position", "absolute");
+	setTimeout(function () {
+		$("h1").animate({
+			left: "400px",
+			opacity: 0
+		}, 1000);
+	}, 3000);
+});
